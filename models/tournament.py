c5f17c4cfb1942f4c7c13d721d8dda0162dcbe71
A'nsi Lema <ansilema@gmail.com>
2024-10-01 10:51:57 +0200

    Ajout de la class Round au model

---
+++ models/tournament.py
@@ -0,0 +1,18 @@
+from models import players
+
+class Tournament:
+    def __init__(self, name, number_of_rounds, location, start_date, end_date, ):
+        self.name = name
+        self.location = location
+        self.start_date = start_date
+        self.end_date = end_date
+        self.number_of_rounds = number_of_rounds
+        self.current_round = 0
+        self.rounds = []
+        self.players = []
+
+    def add_player(self, player):
+        for player in self.players:
+            self.players.append(player)
+
+    
