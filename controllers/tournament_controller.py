f4d7f22fcb1cf4719640e3e7d633ae1058b9b084
A'nsi Lema <ansilema@gmail.com>
2024-10-21 20:54:06 +0200

    Remove debug print and reorganize match play logic
    Eliminate debug print statements and reposition `add_round` invocation to ensure all matches are played before adding the round. This refactor enhances code readability and maintains logical consistency in the `tournament_controller.py` file.

--- controllers/tournament_controller.py
+++ controllers/tournament_controller.py
@@ -43,7 +43,6 @@ def play_round(current_tournament, round_number):
     print(TOUR_LABEL.format(round_number))
     sorted_players = sorted(current_tournament.players, key=lambda player: player[1], reverse=True)
     matches = []
-    print(sorted_players[0][0][0])
     match1 = Match(sorted_players[0][0], sorted_players[1][0])
     match2 = Match(sorted_players[2][0], sorted_players[3][0])
     match3 = Match(sorted_players[4][0], sorted_players[5][0])
@@ -54,14 +53,12 @@ def play_round(current_tournament, round_number):
     round.matches.append(match3)
     round.matches.append(match4)
 
-    current_tournament.add_round(round)
-
     match1.play()
     match2.play()
     match3.play()
-
     match4.play()
 
+    current_tournament.add_round(round)
     for match in round.matches:
         for player in current_tournament.players:
             if player[0] == match.match[0][0]:
@@ -93,10 +90,6 @@ def play_tournament(tournament, number_of_rounds=4):
 
     for round_number in range(1, number_of_rounds + 1):
         play_round(tournament, round_number)
-    # for round_number in range(1, tournament.rounds + 1):
-    play_round(tournament, 1)
-
-    # display_final_results(players)
 
 
 def display_matchups(pairs):
